cfe2332ef18be2fa5f12da40d70978e4c5acf0b7
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-19 22:04:34 +0200

    Added missing code
    lol

--- src/index.jsx
+++ src/index.jsx
@@ -14,3 +14,9 @@ class MyFlixApplication extends React.Component {
     );
   }
 }
+
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
