79178bc84c5f642cb237a319ce14feb37d708b65
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-19 20:39:56 +0200

    Testing

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
+}
