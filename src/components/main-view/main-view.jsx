8c4322530a6cb5c78ebef3e5bed60b38c38f4cf9
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-05-22 03:56:19 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import axios from 'axios';
-
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -9,21 +7,13 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [],
+      movies: [
+        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
+        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+      ],
       selectedMovie: null
     }
-  }
-
-  componentDidMount(){
-    axios.get('https://movime-api.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -35,14 +25,16 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0) return <div className="main-view" />;
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
+  
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
   
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
           ))
         }
       </div>
